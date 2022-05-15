27df3fa9c8cf76a7eab94509e5711bee4449c430
Jan te Beest <jtb@fester.io>
2022-05-15 19:53:50 +0400

    trivial: add some comments

--- graphql-svc/internal/kafka/client.go
+++ graphql-svc/internal/kafka/client.go
@@ -13,6 +13,11 @@ type client struct {
 var cl client
 
 func InitClient() error {
+	// FIXME by generating a new groupid every time this service starts,
+	// it effectively resets the offset and will read all records from 0.
+	// Although they will not be handled (we need to fix this too though,
+	// user_id is not what we should store in the map, use something else),
+	// we should determine the previous offset or use a somewhat static node_id.
 	cl1, err := kgo.NewClient(
 		kgo.SeedBrokers("redpanda.redpanda-system:9092"),
 		kgo.ConsumerGroup("graphql-svc-"+rand.ShortString(4)),
